26df06e4018b9795f0010fe855f718ad24eff174
Christian Baumann <christian.baumann@maibornwolff.de>
2025-09-13 10:13:25 +0200

    Initial commit

---
+++ src/main/java/dev/christianbaumann/App.java
@@ -0,0 +1,13 @@
+package dev.christianbaumann;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
